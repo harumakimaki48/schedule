8c8291ed1069c094f416a75afcb46a50838308e3
harumakimaki48 <haru1301042@outlook.jp>
2024-12-06 01:50:18 +0900

    ADD_setting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_30_080257) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_05_143153) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -147,7 +147,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_30_080257) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user_number", default: "default_number", null: false
+    t.string "user_number", null: false
     t.string "user_name", null: false
     t.integer "role", default: 0, null: false
     t.string "uid"
